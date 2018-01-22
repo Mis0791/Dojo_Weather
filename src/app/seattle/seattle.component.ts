9ed2a6748bc21728d7ffea25cee1ee2e29c69c3f
Michelle Sturgill <isabelsturgill@comcast.net>
2018-01-22 08:28:43 -0800

    Update Temp

--- src/app/seattle/seattle.component.ts
+++ src/app/seattle/seattle.component.ts
@@ -18,8 +18,11 @@ export class SeattleComponent implements OnInit {
       console.log(data);
       this.humidity = data.main.humidity;
       this.tempAvg = data.main.temp;
+      this.tempAvg = Math.floor(this.tempAvg * (9/5) - 459.67);
       this.tempHigh = data.main.temp_max;
+      this.tempHigh = Math.floor(this.tempHigh * (9/5) - 459.67);
       this.tempLow = data.main.temp_min;
+      this.tempLow = Math.floor(this.tempLow * (9/5) - 459.67);
     });
   }
 
